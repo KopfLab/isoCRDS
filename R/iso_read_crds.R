3ae5e5fd8759e66406352c3e69bf76fd06195167
tacaro <51676080+tacaro@users.noreply.github.com>
2024-03-15 14:02:47 -0600

    added ability to modify timezone

--- R/iso_read_crds.R
+++ R/iso_read_crds.R
@@ -1,7 +1,8 @@
-#' @title read Picarro CRDS output .dat file
-#' @description reads Picarro cavity ring down spectroscopy 'CRDS' .dat file
-#' @param path file path to a single CRDS .dat file
-#' @return a tibble of cleaned Picarro data
+#' @title Read Picarro CRDS output .dat file
+#' @description Reads Picarro cavity ring down spectroscopy 'CRDS' .dat file
+#' @param path A character string indicating the file path to a single CRDS .dat file
+#' @param timezone A character string indicating the timezone to read CRDS datetime format. Defaults to UTC if not specified.
+#' @return A tibble of cleaned Picarro data
 #' @examples
 #' # read a data file
 #' iso_read_crds("a_crds_data_file.dat")
@@ -13,16 +14,19 @@
 
 
 # Use this for reading a single CRDS file:
-iso_read_crds <- function(path) {
+iso_read_crds <- function(path, timezone = "UTC") {
   read.delim(path , sep = "", header = TRUE) |>
     dplyr::mutate(
       DATE = lubridate::ymd(DATE), # convert column to date type
       TIME = lubridate::hms(TIME), # convert column to time type
-      datetime = DATE + TIME, # create a single datetime column
+      # convert to datetime format, account for timezone difference
+      datetime = dplyr::case_when(
+        timezone == "UTC" ~ ymd_hms(DATE + TIME),
+        timezone != "UTC" ~ ymd_hms(DATE + TIME, tz = timezone)
+      ),
       filename = path # note the file it was imported from
     ) |>
     dplyr::select(datetime, everything()) # put it at the front for readability
-
 }
 
 
--- R/iso_read_many_crds.R
+++ R/iso_read_many_crds.R
@@ -1,7 +1,8 @@
-#' @title read multiple Picarro CRDS output .dat file
-#' @description reads all Picarro cavity ring down spectroscopy 'CRDS' .dat files from a single directory.
-#' @param directory a folder directory containing multiple .dat files
-#' @return a tibble of cleaned Picarro data
+#' @title Read multiple Picarro CRDS output .dat file
+#' @description Reads all Picarro cavity ring down spectroscopy 'CRDS' .dat files from a single directory.
+#' @param directory A folder directory containing multiple .dat files
+#' @param timezone A character string indicating the timezone to read CRDS datetime format. Defaults to UTC if not specified.
+#' @return A tibble of cleaned Picarro data
 #' @examples
 #' # read a folder's worth of CRDS data
 #' iso_read_many_crds("folder_with_crds_data/")
@@ -17,10 +18,10 @@
 
 
 # For reading multiple files from a single directory:
-iso_read_many_crds <- function(directory) {
+iso_read_many_crds <- function(directory, timezone = "UTC") {
   file_names <- list.files(path = directory, full.names = TRUE)
 
-  combined_tibble <- map(file_names, iso_read_crds) |>
+  combined_tibble <- map(file_names, iso_read_crds(timezone = timezone)) |>
     list_rbind()
 
   return(combined_tibble)
